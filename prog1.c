85b169d5960af7ef300cb3b617f319ea988f611f
cothranj <cothranj@oregonstate.edu>
2026-01-07 08:49:34 -0800

    Assignment Completed

--- prog1.c
+++ prog1.c
@@ -1,5 +1,6 @@
 #include <stdio.h> // Necessary for printf()
-
+#include <stdlib.h>
+#include <time.h>
 // TODO Other includes
 
 
@@ -8,4 +9,13 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	srand(time(NULL));
+	int myrand = rand()%10;
+
+	if (myrand <= 4) {printf("Eat more beef, kick less cats\n");}
+	else if (myrand <= 9 ) {printf("FRODO LIVES\n");}
+	else {printf("Larn is the best roguelike\n");}
+
+	printf("The random number was: %d\n", myrand);
+	return 0;
 }
